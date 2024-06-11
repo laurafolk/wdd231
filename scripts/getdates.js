b731e7daf145500a7e5ab25154ce15e124c4c579
laurafolk <laurafolk@yahoo.com>
2024-06-10 20:20:45 -0500

    Week01 Course Home Page layout.

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //Gets the current year
+    var currentYear = new Date().getFullYear();
+
+    //Displays the current year in the HTML span element with id "currentYear"
+    document.getElementById("currentYear").textContent = currentYear;
+
+    //Gets the last modified date of the document
+    var lastModifiedDate = new Date(document.lastModified);
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var formattedDate = lastModifiedDate.toLocaleDateString('en-us', options);
+
+    document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate;
+});
