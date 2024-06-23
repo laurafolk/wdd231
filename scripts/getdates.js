d98db5b943d3b659d1ddfebdca5645576272e867
laurafolk <laurafolk@yahoo.com>
2024-06-22 21:20:12 -0500

    Updated getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
     //Gets the current year
-    var currentYear = new Date().getFullYear();
+    let currentYear = new Date().getFullYear();
+    
 
     //Displays the current year in the HTML span element with id "currentYear"
     document.getElementById("currentYear").textContent = currentYear;
 
     //Gets the last modified date of the document
-    var lastModifiedDate = new Date(document.lastModified);
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    var formattedDate = lastModifiedDate.toLocaleDateString('en-us', options);
+    let lastModifiedDate = document.lastModified;
 
-    document.getElementById("lastModified").innerHTML = "Last Modified: " + formattedDate;
-});
+
+    document.getElementById("lastModified").innerHTML = "Last Modified: " + lastModifiedDate;
